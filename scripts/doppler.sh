b8d8737dab3aad5d014655eb85e799508d0d737d
janze1992 <125573413+janze1992@users.noreply.github.com>
2025-09-05 22:57:27 +0200

    update

--- scripts/doppler.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mpv --stream-buffer-size=2m --loop-file=inf --wid=$(xdotool getactivewindow) "https://radar.weather.gov/lite/N0R/ATX_loop.gif"
--- scripts/restartDunst.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pkill -9 dunst
-notify-send "Dunst Restarted"
